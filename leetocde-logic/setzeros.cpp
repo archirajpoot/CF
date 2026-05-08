2fdf87946d4b51dbcd1a0b0b8527e1894d56f488
archisinghrajput <archirajpoot21@gmail.com>
2026-05-09 01:02:19 +0530

    logic

--- leetocde-logic/setzeros.cpp
+++ leetocde-logic/setzeros.cpp
@@ -1,32 +1,32 @@
-class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
         
-        int n = matrix.size();
-        int m = matrix[0].size();
+//         int n = matrix.size();
+//         int m = matrix[0].size();
 
-        vector<int> rows(n,0);
-        vector<int> cols(m,0);
+//         vector<int> rows(n,0);
+//         vector<int> cols(m,0);
 
-        // mark rows and cols
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+//         // mark rows and cols
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
 
-                if(matrix[i][j]==0){
-                    rows[i]=1;
-                    cols[j]=1;
-                }
-            }
-        }
+//                 if(matrix[i][j]==0){
+//                     rows[i]=1;
+//                     cols[j]=1;
+//                 }
+//             }
+//         }
 
-        // set zeroes
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+//         // set zeroes
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
 
-                if(rows[i]==1 || cols[j]==1){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-    }
-};
+//                 if(rows[i]==1 || cols[j]==1){
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+//     }
+// };
